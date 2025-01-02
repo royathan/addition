c2688d86af6d382ad400e799561837d0ed164110
Roy Smith <rsmith@veracode.com>
2025-01-02 12:35:54 -0500

    generic add

--- addition.go
+++ addition.go
@@ -1,9 +1,17 @@
 package addition
 
+import (
+	"golang.org/x/exp/constraints"
+)
+
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
 // Adds two integers together via [addition].
 //
 // [addition]: https://www.mathsisfun.com/numbers/addition.html
-func Add(a, b int) int {
+func Add[T Number](a, b T) T {
 	return a + b
 }
 
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module github.com/royathan/addition/v2
+module github.com/royathan/addition/v3
 
 go 1.23.4
+
+require golang.org/x/exp v0.0.0-20241217172543-b2144cdd0a67
